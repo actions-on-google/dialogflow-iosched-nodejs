8fa47e74288bcba46bc2283facd9feb9e701e099
Taylor Caldwell <taycaldwell@google.com>
2018-05-15 15:09:56 -0700

    Add more to init call
    Bug: 78653572
    Change-Id: I38b35957cfe4ff744a59e5dc2fc6ad7869ab1e73

--- functions/app.js
+++ functions/app.js
@@ -53,6 +53,12 @@ app.middleware((conv) => {
     conv.data.noInputCount = 0;
     conv.data.fallbackResponses = [];
     conv.data.noInputResponses = [];
+    conv.data.currentItems = [];
+    conv.data.nextItems = [];
+    conv.data.sessionType = null;
+    conv.data.sessionShown = null;
+    conv.data.sessionsTag = null;
+    conv.data.tagId = null;
   }
   conv.conference = new ConferenceData();
   conv.screen = conv.surface.capabilities
