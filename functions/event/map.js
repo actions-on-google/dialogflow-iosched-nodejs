ab1f7735e27cdb56936986a10ee8da5b1ed70c1c
Taylor Caldwell <taycaldwell@google.com>
2018-05-16 10:55:15 -0700

    Remove unused image data from map JSON
    Change-Id: I74a98fc5921157bee5d22818f7d17d5d30822349

--- functions/event/map.js
+++ functions/event/map.js
@@ -16,206 +16,166 @@
 exports.rooms = {
   'ab4d1ac3-f7e2-4bb3-a255-321c2c6ccad3': {
     name: 'Lost and Found',
-    image: 'lost_and_found.png',
     directions: 'The lost and found station is located at the Conference Help Desk during event hours. Any items left overnight will be turned over to the Conference Security Office. The Help Desk is located in front of the Codelabs building, past the I/O sign.',
   },
   'cd4a2dd1-c4b3-4cd3-a471-342a2b6eead1': {
     name: 'Bathroom',
-    image: 'bathrooms.png',
     directions: `Restrooms and Mother's Rooms are located all around the event. You can find them on either side of the Amphitheatre and Codelabs building. Other restrooms can be found near Stage 8 on the Northside of the lot, near Tent C on the Westside of the lot, or near Tent D and Tent E on the Eastside of the lot.`,
   },
   'fb8f9af2-c4b3-4cd3-b571-421e2c6ebad1': {
     name: 'Amphitheatre',
-    image: 'amphitheatre.png',
     directions: `The Shoreline Amphitheatre is the main stage of Google I/O. You can spot it by it's large white canopies and it's lawn where viewers can watch the Keynote and latest announcements. To get to the Amphitheatre, walk straight ahead from the entrance, past the info desk. You can't miss it!`,
   },
   '4902fc2b-dbf1-49dc-828f-2566dde55e03': {
     name: 'App Reviews tent - B',
-    image: 'tent_b.png',
     directions: 'Tent B can be found on the Westside lot of the event. If you take a left from the entrance, it will be the second dome shaped tent you see near the Community Lounge. Residing between Tent A and Stage 3.',
   },
   'b5a545c7-20cb-4d88-8752-a08374c64d00': {
     name: 'App Reviews tent - C',
-    image: 'tent_c.png',
     directions: 'To find Tent C, take a left from the entrance and cruise all the way down Main Street. Tent C will be on your right, next to the EATs Market and Android Auto Sandbox.',
   },
   'd01912b6-5851-453d-b7ef-178a83d47d16': {
     name: 'App Reviews tent - F',
-    image: 'tent_f.png',
     directions: 'Tent F can be found on the Eastside lot of the event. If you take a left from the entrance and take a short stroll down Main Street, it will be the second dome shaped tent on your left, near the Waymo Sandbox.',
   },
   'c82507f9-e904-49e3-a1ce-9b95a97e816d': {
     name: 'Codelabs building',
-    image: 'codelabs_building.png',
     directions: 'The Codelabs building is located right behind the Amphitheatre. If you keep walking straight from the entrance and pass the info desk, you will find yourself at the Codelabs building.',
   },
   'ce2a604e-bcf0-497e-a212-36403c01da1f': {
     name: 'Community lounge',
-    image: 'community_lounge.png',
     directions: 'To find the Community Lounge, walk left from the entrance past Tent A and the Certification Lounge.',
   },
   '0faf0b5a-6379-41f5-9d37-b897857ebf09': {
     name: 'EATS market',
-    image: 'eats_market.png',
     directions: 'If you take a left from the entrance and cruise all the way down Main Street. The EATS Market will be on your right, next to Tent C and the Android Auto Sandbox.',
   },
   '214ba1c8-b820-4ea7-ad43-7cf0dfa96ed6': {
     name: 'Main street',
-    image: 'main_street.png',
     directions: 'Main Street is the pathway perpendicular to the entrance of the event. It passes through the Eastside parking lot of the event, where you can find various Tents and the Waymo Sandbox!',
   },
   'c0814e69-05b8-4889-b62d-63c0fc9c8889': {
     name: 'Near Sandboxes A and Tent B',
-    image: 'tent_a.png',
     directions: 'Tent A and Tent B can be found on the Westside lot of the event. If you take a left from the entrance, they will be the first two dome shaped tents you see near the Community Lounge. Residing near Stage 1.',
   },
   '7b8119cb-f45f-4e6e-88bc-7ab636a56ede': {
     name: 'Office Hours tent - A',
-    image: 'tent_a.png',
     directions: 'Tent A can be found on the Westside lot of the event. If you take a left from the entrance, it will be the first dome shaped tent you see near the Community Lounge. Residing near Stage 1 and Tent B.',
   },
   'c964e4db-d3d1-40fe-a62d-50a1ca5a80f5': {
     name: 'Office Hours tent - B',
-    image: 'tent_b.png',
     directions: 'Tent B can be found on the Westside lot of the event. If you take a left from the entrance, it will be the second dome shaped tent you see near the Community Lounge. Residing between Tent A and Stage 3.',
   },
   '652c0038-28f3-4a82-9ba1-246584d14834': {
     name: 'Office Hours tent - C',
-    image: 'tent_c.png',
     directions: 'To find Tent C, take a left from the entrance and cruise all the way down Main Street. Tent C will be on your right, next to the EATs Market and Android Auto Sandbox.',
   },
   '70635fce-7fee-42d8-a288-850a4130fcfd': {
     name: 'Office Hours tent - D',
-    image: 'tent_d.png',
     directions: 'To find Tent D, take a left from the entrance and walk all the way down Main Street. Tent D will be on your left, neighboring Stage 5.',
   },
   'e97ebb11-69a1-4fad-b8ff-dcffad23c55e': {
     name: 'Office Hours tent - E',
-    image: 'tent_e.png',
     directions: 'Tent E can be found on the Eastside lot of the event. If you take a left from the entrance and walk a little ways down Main Street, it will be the dome shaped tent on your left, past Tent F.',
   },
   '08978718-3431-4b51-8998-262a0093fcf8': {
     name: 'Office Hours tent - F',
-    image: 'tent_f.png',
     directions: 'Tent F can be found on the Eastside lot of the event. If you take a left from the entrance and take a short stroll down Main Street, it will be the second dome shaped tent on your left, near the Waymo Sandbox.',
   },
   '25ed86fb-9afd-4568-9e20-9e7d6abe12d9': {
     name: 'Outside of EATS Market',
-    image: 'eats_market.png',
+    directions: 'If you take a left from the entrance and cruise all the way down Main Street. The EATS Market will be on your right, next to Tent C and the Android Auto Sandbox.',
   },
   '4def97db-565e-4170-903f-0c10733e1795': {
     name: 'Outside of Stage 1',
-    image: 'stage_1.png',
     directions: 'Stage 1 is next to Tent A and Tent B. To get there, take a left from the entrance and keep walking till you see it on your left!',
   },
   'bd1ca1f1-baa7-4daf-9c49-e8342b2f6c5c': {
     name: 'Outside of Stage 5',
-    image: 'stage_5.png',
     directions: 'To find Stage 5, take a left from the entrance and walk all the way down Main Street. Stage 5 will be on your left, neighboring Tent D.',
   },
   '8cc29f54-3e03-4265-8ca1-fb7b7c6bdf62': {
     name: 'Outside of Stage 6',
-    image: 'stage_6.png',
     directions: 'Stage 6 can be found on the Eastside lot of the event. If you take a left from the entrance and take a short stroll down Main Street, it will be the stage between two dome tents on your left, near the Waymo Sandbox.',
   },
   'c5d1d7f1-e3ee-47ab-9aaa-b2e52661c902': {
     name: 'Outside of Stage 7',
-    image: 'stage_7.png',
     directions: 'Stage 7 can be found on the Northside lot of the event. If you take a right from the entrance it will be the first stage you come across.',
   },
   '5873aa06-16aa-4ac5-a727-5c72c6912846': {
     name: 'Porch outside Stage 1',
-    image: 'stage_1.png',
     directions: 'Stage 1 is next to Tent A and Tent B. To get there, take a left from the entrance and keep walking till you see it on your left!',
   },
   '869aff8a-e348-44f7-8ddd-8c7dafa61877': {
     name: 'Sandbox A',
-    image: 'tent_a.png',
     directions: 'Tent A can be found on the Westside lot of the event. If you take a left from the entrance, it will be the first dome shaped tent you see near the Community Lounge. Residing near Stage 1 and Tent B.',
   },
   'a95018c9-2e7c-41c7-acc9-757286a26fba': {
     name: 'Sandbox B',
-    image: 'tent_b.png',
     directions: 'Tent B can be found on the Westside lot of the event. If you take a left from the entrance, it will be the second dome shaped tent you see near the Community Lounge. Residing between Tent A and Stage 3.',
   },
   '58412c3f-d169-4d74-9c52-5b62811f5c5e': {
     name: 'Sandbox C',
-    image: 'tent_c.png',
     directions: 'To find Tent C, take a left from the entrance and cruise all the way down Main Street. Tent C will be on your right, next to the EATs Market and Android Auto Sandbox.',
   },
   'd204e623-742f-456e-8cdf-2ed4f6b55562': {
     name: 'Sandbox D',
-    image: 'tent_d.png',
     directions: 'To find Tent D, take a left from the entrance and walk all the way down Main Street. Tent D will be on your left, neighboring Stage 5.',
   },
   '3f731d37-ef84-4fb5-b24e-78e9b52e887f': {
     name: 'Sandbox E',
-    image: 'tent_e.png',
     directions: 'Tent E can be found on the Eastside lot of the event. If you take a left from the entrance and walk a little ways down Main Street, it will be the dome shaped tent on your left, past Tent F.',
   },
   'ae0ffded-3aaa-4a4b-b4ee-9840c5284cae': {
     name: 'Sandbox F',
-    image: 'tent_f.png',
     directions: 'Tent F can be found on the Eastside lot of the event. If you take a left from the entrance and take a short stroll down Main Street, it will be the second dome shaped tent on your left, near the Waymo Sandbox.',
   },
   'eb3fea16-5fc5-48c7-a7b5-9133d0506b9a': {
     name: 'Sandbox G',
-    image: 'tent_g.png',
     directions: 'Tent F can be found on the Eastside lot of the event. If you take a left from the entrance and take a short stroll down Main Street, it will be the second dome shaped tent on your left, near the Waymo Sandbox.',
   },
   '7266203e-af27-40e7-879c-dc4ef7698bb5': {
     name: 'Sandbox H',
-    image: 'tent_h.png',
     directions: 'Tent H can be found on the Northside lot of the event. If you take a right from the entrance it will be the first dome shaped tent you encounter, past Stage 7.',
   },
   '31568d31-025d-4c18-9d4f-4bb5455692bf': {
     name: 'Sandbox I',
-    image: 'tent_i.png',
     directions: 'Tent I can be found on the Northside lot of the event. If you take a right from the entrance it will be the second dome shaped tent you see, neighboring the Office Hours and App Reviews tent.',
   },
   'fc6ef5c6-b22e-4e6f-b2ff-5779ce64d042': {
     name: 'Stage 1',
-    image: 'stage_1.png',
     directions: 'Stage 1 is next to Tent A and Tent B. To get there, take a left from the entrance and keep walking till you see it on your left!',
   },
   'd98e5448-b232-4117-80b8-5ebe0aa24d77': {
     name: 'Stage 2',
-    image: 'stage_2.png',
     directions: 'Stage 2 is right next to Stage 1. To find it, take a left from the entrance and keep walking till you see Stage 1 on your left. Keeping walking past Stage 1, and you will find yourself at Stage 2.',
   },
   '38c52143-97c0-4e53-8c24-06e7aaccb4ba': {
     name: 'Stage 3',
-    image: 'stage_3.png',
     directions: 'To find Stage 3, take a left from the entrance towards the Westside parking lot till you see Tent A and Tent B on your left. Turn left at Tent A and keep walking till you find yourself at Stage 3.',
   },
   '3d3e571f-dc26-45f1-8ef9-17bcf432a227': {
     name: 'Stage 4',
-    image: 'stage_4.png',
     directions: 'To find Stage 4, take a left from the entrance and cruise all the way down Main Street. Stage 4 will be on your right, next to the Android Auto Sandbox.',
   },
   '5efaf351-163c-4b7f-a7a1-7e1ec93f235b': {
     name: 'Stage 5',
-    image: 'stage_5.png',
     directions: 'To find Stage 5, take a left from the entrance and walk all the way down Main Street. Stage 5 will be on your left, neighboring Tent D.',
   },
   'a49cbb83-6008-4468-90e3-418dbbf2341f': {
     name: 'Stage 6',
-    image: 'stage_6.png',
     directions: 'Stage 6 can be found on the Eastside lot of the event. If you take a left from the entrance and take a short stroll down Main Street, it will be the stage between two dome tents on your left, near the Waymo Sandbox.',
   },
   '3570bda2-0bc3-4b3b-a7c7-8514f8026ab3': {
     name: 'Stage 7',
-    image: 'stage_7.png',
     directions: 'Stage 7 can be found on the Northside lot of the event. If you take a right from the entrance it will be the first stage you come across.',
   },
   '9c37194e-fe72-4218-8e23-fd296eed747f': {
     name: 'Stage 8',
-    image: 'stage_8.png',
     directions: 'Stage 8 can be found on the Northside lot of the event. If you take a right from the entrance and keep walking, it will be the stage past Tent I.',
   },
   'f97804c1-56d2-4c2c-a70b-7e70011ffb2a': {
     name: 'Totem',
-    image: 'totem.png',
     directions: 'The information desk can be found at the entrance of the event.',
   },
 };
